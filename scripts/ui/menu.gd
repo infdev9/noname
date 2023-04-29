90b8846644340a60bcb3887f4a976bce6b042e0e
unreality9 <unreality9@users.noreply.github.com>
2023-04-29 23:57:56 +0700

    Partially integrated menu

--- scripts/ui/menu.gd
+++ scripts/ui/menu.gd
@@ -5,5 +5,19 @@ func _on_play_pressed() -> void:
 	Root.change_scene(GLOBAL.SCENES.GAME)
 
 
+func _on_settings_pressed() -> void:
+	pass
+
+
 func _on_exit_pressed() -> void:
 	Root.quit()
+
+
+func _on_settings_hovered() -> void:
+	$Background/Glitch.hide()
+	$Background/SettingsGlitch.show()
+
+
+func _on_settings_unhovered() -> void:
+	$Background/Glitch.show()
+	$Background/SettingsGlitch.hide()
